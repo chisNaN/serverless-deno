b7ec10cc395e633d935762a341a011cb13a6aa15
₳ЯṬ <artgreg@outlook.fr>
2020-05-10 13:24:32 +0200

    test cors

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -24,6 +24,7 @@ export async function handler (req: object) {
     return {
     headers: {
       //'content-type': 'text/html; charset=utf8',
+	    'cors': true,
       'Access-Control-Allow-Origin': '*',
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
